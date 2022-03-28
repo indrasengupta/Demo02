806aa26b0022196a3059ef50e142e98d8a844368
indrasengupta <101565868+indrasengupta@users.noreply.github.com>
2022-03-28 15:49:55 +0530

    Add files via upload

---
+++ Q2.java
@@ -0,0 +1,8 @@
+//Q 2 write a program that defines two variables and print their sum .
+
+public class Q2 {
+public static void main(String[] args) {
+	int a=10,b=20;
+	System.out.println("Sum= "+(a+b));
+}
+}
